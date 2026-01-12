86555f3c436bde127bbe970845a45f858d457e18
Kamil Kujawa <kujawakamil.dev@gmail.com>
2026-01-12 22:57:37 +0100

    dashboard fix

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -16,11 +16,11 @@ return {
 		}
 
 		dashboard.section.buttons.val = {
-			dashboard.button("f", "󰈞  Find file", "<leader>ff"),
-			dashboard.button("p", "  Projects", "<leader>fp"),
-			dashboard.button("r", "  Recent files", "<leader>fr"),
-			dashboard.button("n", "  New file", ":ene <BAR> startinsert <CR>"),
-			dashboard.button("q", "󰅚  Quit", ":qa<CR>"),
+			dashboard.button("f", "󰈞  Find file", "<cmd>Telescope find_files<CR>"),
+			dashboard.button("p", "  Projects", "<cmd>Telescope projects<CR>"),
+			dashboard.button("r", "  Recent files", "<cmd>Telescope oldfiles<CR>"),
+			dashboard.button("n", "  New file", "<cmd>ene <BAR> startinsert<CR>"),
+			dashboard.button("q", "󰅚  Quit", "<cmd>qa<CR>"),
 		}
 
 		alpha.setup(dashboard.config)
